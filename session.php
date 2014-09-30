3f6593ea8af341176f4c064f5290151fd9858506
gskoropada <gskoropada@gmail.com>
2014-09-30 15:42:38 +1000

    Added information about session variables

--- session.php
+++ session.php
@@ -1,6 +1,11 @@
 <?php
 /*
  * SESSION start and timeout procedure.
+ * $_SESSION['userid'] Represents the UserID as stored in the Database
+ * $_SESSION['screenname'] User's Screen name
+ * $_SESSION['role'] Stores user role as defined in the Database
+ * $_SESSION['avatar'] Stores a unique identifier refering to the User's picture located in profile_pics\
+ * $_SESSION['pwdRst'] Flag indicating that the user must change the password because it has been recently reset;
  */
 $timeout = 3600; //Expressed in seconds
 
